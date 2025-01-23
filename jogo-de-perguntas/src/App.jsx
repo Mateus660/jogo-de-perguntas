bab9760b83d5ed5f28889f593dcbd85df79f591a
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-01-22 23:11:17 -0300

    atualizei o projeto

---
+++ jogo-de-perguntas/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Jogo from './assets/jogo';
+
+function App() {
+  return (
+    <div>
+      <Jogo />
+    </div>
+  );
+}
+
+export default App;
---
+++ jogo-de-perguntas/src/assets/jogo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Jogo() {
+  return (
+    <div>
+      <h1>Bem vindo ao jogo</h1>
+    </div>
+  );
+}
+
+export default Jogo;
