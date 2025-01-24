578e4406a82447c155675a99abeea4928e93750e
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-01-23 23:09:35 -0300

    projeto atualizado

--- jogo-de-perguntas/src/assets/jogo.jsx
+++ jogo-de-perguntas/src/assets/jogo.jsx
@@ -4,6 +4,11 @@ function Jogo() {
   return (
     <div>
       <h1>Bem vindo ao jogo</h1>
+      <div id='cadastro'>
+        <p>Qual é o seu nome ?</p>
+        <input type="text" name="nome" id="nome" placeholder='Escreva seu nome' />
+        <button type="submit" id='enviar'>Enviar</button>
+      </div>
     </div>
   );
 }
